c6a6e28c9c5c638796918793ad18f7fcf89bc023
AhmadZaid20187 <ahmadzaid20187@gmail.com>
2026-02-24 14:12:49 +0600

    seems like done

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let interviewList = [];
 let rejectedList = [];
+let currentStatus = 'all'
 
 let total = document.getElementById('total');
 let interview = document.getElementById('interview');
@@ -35,14 +36,21 @@ function toggleStyle(id) {
 
     // After Click
     const selected = document.getElementById(id)
+    currentStatus = id
+
     selected.classList.add('bg-[#1A77F2]', 'text-white')
 
     if (id == 'interview-filter-btn') {
         allJobsSection.classList.add('hidden');
         filteredSection.classList.remove('hidden');
+        renderInterview();
     } else if (id == 'all-filter-btn') {
         allJobsSection.classList.remove('hidden');
         filteredSection.classList.add('hidden');
+    } else if (id == 'rejected-filter-btn') {
+        allJobsSection.classList.add('hidden');
+        filteredSection.classList.remove('hidden');
+        renderReject();
     }
 
 }
@@ -66,6 +74,7 @@ mainContainer.addEventListener('click', function (event) {
         parentNode.querySelector('.staTus').classList.add('badge-accent')
         parentNode.querySelector('.staTus').classList.add('text-white')
         parentNode.querySelector('.staTus').classList.remove('bg-gray-300')
+        parentNode.querySelector('.staTus').classList.remove('badge-error')
 
         const cardInfo = {
             cardTitle,
@@ -82,7 +91,53 @@ mainContainer.addEventListener('click', function (event) {
         if (!CardExist) {
             interviewList.push(cardInfo)
         }
-        renderInterview()
+
+        rejectedList = rejectedList.filter(item => item.cardTitle != cardInfo.cardTitle)
+
+        if (currentStatus == 'rejected-filter-btn') {
+            renderReject()
+        }
+
+        // renderInterview()
+        calculateCount()
+    } else if (event.target.classList.contains('reject-btn')) {
+        const parentNode = event.target.parentNode.parentNode;
+
+        const cardTitle = parentNode.querySelector('.cardTitle').innerText
+        const positionName = parentNode.querySelector('.positionName').innerText
+        const jobDes = parentNode.querySelector('.jobDes').innerText
+        const staTus = parentNode.querySelector('.staTus').innerText
+        const nodes = parentNode.querySelector('.nodes').innerText
+
+        // Status: REJECTED
+        parentNode.querySelector('.staTus').innerText = "REJECTED"
+        parentNode.querySelector('.staTus').classList.add('badge-error')
+        parentNode.querySelector('.staTus').classList.add('text-white')
+        parentNode.querySelector('.staTus').classList.remove('bg-gray-300')
+
+        const cardInfo = {
+            cardTitle,
+            positionName,
+            jobDes,
+            staTus: 'REJECTED',
+            nodes
+
+        }
+
+        const CardExist = rejectedList.find(item => item.cardTitle == cardInfo.cardTitle)
+
+
+        if (!CardExist) {
+            rejectedList.push(cardInfo)
+        }
+
+        interviewList = interviewList.filter(item => item.cardTitle != cardInfo.cardTitle)
+
+        if (currentStatus == 'interview-filter-btn') {
+            renderInterview()
+        }
+
+        calculateCount()
     }
 
 })
@@ -114,8 +169,48 @@ function renderInterview() {
                             millions of users worldwide.</p>
                     </div>
                     <div class=" flex gap-3">
-                        <button class="btn btn-outline btn-accent">INTERVIEW</button><button
-                            class="btn btn-outline btn-error">REJECTED</button>
+                        <button class="interview-btn btn btn-outline btn-accent">INTERVIEW</button><button
+                            class="reject-btn btn btn-outline btn-error">REJECTED</button>
+                    </div>
+
+                </div>
+                <!-- main part-2 -->
+                <div>
+                    <button class="btn rounded-full btn-outline"><i class="fa-solid fa-trash"></i></button>
+                </div>
+        `
+        filteredSection.appendChild(div)
+    }
+}
+
+function renderReject() {
+    filteredSection.innerHTML = ''
+
+
+    for (let reject of rejectedList) {
+        console.log(interview)
+
+        let div = document.createElement('div')
+        div.className = 'bg-white flex justify-between p-3 rounded-2xl'
+
+        div.innerHTML = `
+        <div class="space-y-6">
+                    <!-- part 1 -->
+                    <div>
+                        <h1 class="cardTitle text-xl text-[#002C5C] font-semibold">${reject.cardTitle}</h1>
+                        <p class="positionName">React Native Developer</p>
+                        <p class="jobDes">Remote • Full-time • $130,000 - $175,000</p>
+                    </div>
+                    <!-- part 2 -->
+                    <div>
+                        <p class="staTus">${reject.staTus}</p>
+                        <p class="nodes">Build cross-platform mobile applications using React Native. Work on products
+                            used by
+                            millions of users worldwide.</p>
+                    </div>
+                    <div class=" flex gap-3">
+                        <button class="interview-btn btn btn-outline btn-accent">INTERVIEW</button><button
+                            class="reject-btn btn btn btn-outline btn-error">REJECTED</button>
                     </div>
 
                 </div>
